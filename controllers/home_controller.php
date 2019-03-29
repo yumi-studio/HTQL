041bae2338e217e413fbc75af4c67e157415f1df
Yumi <caubedondoc.ls01@gmail.com>
2019-03-29 21:52:17 +0700

    Update home_controller.php

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -14,7 +14,8 @@ class HomeController extends BaseController{
     }
     public function login(){
         if(isset($_SESSION['status'])){
-            header('location: ?controller=home&action=employee');
+            echo $_SESSION['status'];
+            // header('location: ?controller=home&action=employee');
         }else{
             $this->render('login');
         }
@@ -27,13 +28,15 @@ class HomeController extends BaseController{
         if(isset($_POST['btnLogin'])){
             $username = $_POST['txtUsername'];
             $password = $_POST['txtPassword'];
-            $query = "SELECT TOP 1 id,name,status,branch_id FROM account WHERE username=:u AND password=:p";
+            $query = "SELECT top 1 id,name,status,branch_id FROM account WHERE username=:u AND password=:p";
             $stmt = DB::getInstance()->prepare($query);
             $stmt->bindParam(':u',$username);
             $stmt->bindParam(':p',$password);
             $stmt->execute();
-            if($stmt->execute()){
-                $row=$stmt->fetch(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $count = sizeof($result);
+            if($count>0){
+                $row=$result[0];
                 // if($row['password']==$password){
                 $_SESSION['name']=$row['name'];
                 $_SESSION['status']=$row['status'];
@@ -42,13 +45,13 @@ class HomeController extends BaseController{
                 if($row['status']==0 || $row['status']==1){
                     header('location: ?controller=home&action=employee');
                 }else{
-                    header('location: ?controller=account&action=view');
+                    header('location: ?controller=account&action=view&id='.$row['id']);
                 }
             }else{
-                header('location: ?controller=account&action=login');
+                header('location: ?controller=home&action=login');
             }
         }else{
-            header('location: ?controller=account&action=login');
+            header('location: ?controller=home&action=login');
         }
     }
     public function employee(){
